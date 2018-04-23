68ba5cc262ee227cfad586472d7384e679dcc083
abhisingh1510 <abhisona2u@gmail.com>
2018-04-24 02:55:17 +0530

    Complete GUI

---
+++ app/src/main/java/com/abhisheksingh/ifbservices/AdministratorPage.java
@@ -0,0 +1,13 @@
+package com.abhisheksingh.ifbservices;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdministratorPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_administrator_page);
+    }
+}
