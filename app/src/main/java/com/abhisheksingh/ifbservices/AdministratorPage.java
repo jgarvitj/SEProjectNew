e698db8cce3e9440e286033bddb0032ec86acea1
Abhishek Pandey <pandeyabhishek2301@gmail.com>
2018-04-25 00:48:16 +0530

    FirstPageDone

--- app/src/main/java/com/abhisheksingh/ifbservices/AdministratorPage.java
+++ app/src/main/java/com/abhisheksingh/ifbservices/AdministratorPage.java
@@ -1,7 +1,10 @@
 package com.abhisheksingh.ifbservices;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AdministratorPage extends AppCompatActivity {
 
@@ -9,5 +12,28 @@ public class AdministratorPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_administrator_page);
+        Button assignCalls=findViewById(R.id.assignCalls_btn);
+        Button billing=findViewById(R.id.billing_btn);
+        Button logout=findViewById(R.id.logout_btn);
+        assignCalls.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent( AdministratorPage.this, AssignCallsPage.class));
+            }
+        });
+        billing.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                //startActivity(new Intent(FirstPage.this, AdministratorPage.class));
+            }
+        });
+        logout.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent( AdministratorPage.this, FirstPage.class));
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/abhisheksingh/ifbservices/FirstPage.java
+++ app/src/main/java/com/abhisheksingh/ifbservices/FirstPage.java
@@ -2,6 +2,14 @@ package com.abhisheksingh.ifbservices;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.view.View.OnClickListener;
+
 
 public class FirstPage extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class FirstPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
+        Button Administrator=findViewById(R.id.administrator_login_btn);
+        Button Employee=findViewById(R.id.employee_login_btn);
+        Button Guest=findViewById(R.id.guest_login_btn);
+
+        Administrator.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(FirstPage.this, AdministratorPage.class));
+            }
+        });
+        Employee.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(FirstPage.this, EmployeePage.class));
+            }
+        });
+        Guest.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v)
+            {
+                startActivity(new Intent(FirstPage.this, GuestPage.class));
+            }
+        });
     }
 }
